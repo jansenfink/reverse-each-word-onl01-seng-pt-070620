0ee7cc4047aaff381267c7815df0b2c75702018c
Jansen Fink <jansenfink@charter.net>
2020-07-18 14:00:56 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+# def reverse_each_word(string)
+#   array = string.split(' ')
+#   new_array = []
+#   array.collect do |element|
+#   new_array << element.reverse()
+#   end 
+# new_array.join(' ')  
+# end  
+
+def reverse_each_word(string)
+  array = string.split(' ')
+  new_array = array.collect do |element|
+    element.reverse()
+  end 
+ new_array.join(' ')  
+end
