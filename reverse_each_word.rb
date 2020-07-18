77516725731cae8ea2425dd59dcb3d7e81e957d9
Jansen Fink <jansenfink@charter.net>
2020-07-18 13:54:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   array = string.split(' ')
+  new_array = []
   array.collect do |element|
-    element = array
-    element.reverse()
+   new_array << element.reverse()
   end 
-  
+ new_array.join(' ')  
 end  
